a0a95981d1cf28357fe51ba6b34b7ad48f12c597
Dylan Fernandes <majestickookie@gmail.com>
2022-09-20 23:30:11 +0100

    Implemented toString method to return the object in a uniform manner and converted PokemonSprite string to File type.

--- src/main/java/PokemonData.java
+++ src/main/java/PokemonData.java
@@ -1,10 +1,14 @@
-import me.sargunvohra.lib.pokekotlin.model.PokemonSprites;
-import me.sargunvohra.lib.pokekotlin.model.PokemonType;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 // Create an object with the data retrieved from the JSON
 class PokemonData {
-
 
     // Attributes required for each Pokémon
     private int pokemonId;
@@ -24,40 +28,55 @@ class PokemonData {
         return pokemonId;
     }
 
-    public void setPokemonId(int pokemonId) {
-        this.pokemonId = pokemonId;
-    }
-
     public String getPokemonName() {
+        pokemonName = pokemonName.substring(0,1).toUpperCase() + pokemonName.substring(1).toLowerCase();
         return pokemonName;
-    }
-
-    public void setPokemonName(String pokemonName) {
-        this.pokemonName = pokemonName;
     }
 
     public List<String> getPokemonTypes() {
         return pokemonTypes;
     }
 
-    public void setPokemonTypes(List<String> pokemonTypes) {
-        this.pokemonTypes = pokemonTypes;
-    }
-
     public List<String> getPokemonSprites() {
         return pokemonSprites;
     }
 
-    public void setPokemonSprites(List<String> pokemonSprites) {
-        this.pokemonSprites = pokemonSprites;
+    // Converts the image URL String to an image
+    public List<File> getPokemonSpritesToFile() throws URISyntaxException {
+
+        //
+        List<File> spriteFiles = new ArrayList<File>();
+
+            try
+            {
+                //Converting string to image and then writing the image into a File
+                Image frontSprite = ImageIO.read(new URL(pokemonSprites.get(0)));
+                Image frontShinySprite = ImageIO.read(new URL(pokemonSprites.get(1)));
+                ImageIO.write((RenderedImage) frontSprite, "png", new File("C://Users/dylan/Desktop/Pokemon Sprites/frontSprite.png"));
+                ImageIO.write((RenderedImage) frontShinySprite, "png", new File("C:/Users/dylan/Desktop/Pokemon Sprites/frontShinySprite.png"));
+
+                // Saving image to local file storage
+                File frontSpriteImage = new File("C://Users/dylan/Desktop/Pokemon Sprites/frontSprite.png");
+                File frontShinySpriteImage = new File("C:/Users/dylan/Desktop/Pokemon Sprites/frontShinySprite.png");
+
+                spriteFiles.add(0, frontSpriteImage);
+                spriteFiles.add(1, frontShinySpriteImage);
+            }
+            catch (Exception e)
+            {
+                System.err.println("Error writing image to a file!");
+            }
+
+        return spriteFiles;
     }
+
 
     public String toString(){
         if (getPokemonTypes().size() > 1){
-            return ("The Pokemon is " + getPokemonName() + " with an ID of " + getPokemonId() + "\n Which is a " + getPokemonTypes().get(0) + " and " + getPokemonTypes().get(1) + " type.");
+            return ("The Pokemon is " + getPokemonName() + ", a " + getPokemonTypes().get(0) + " and " + getPokemonTypes().get(1) + " type." + "\nWith an ID of " + getPokemonId() );
 
         }
-        return ("The Pokemon is " + getPokemonName() + " with an ID of " + getPokemonId() + "\n Which is a " + getPokemonTypes().get(0) + " type.");
+        return ("The Pokemon is " + getPokemonName() + ", a " + getPokemonTypes().get(0) + " type." + "\nWith an ID of " + getPokemonId() );
 
     }
 
