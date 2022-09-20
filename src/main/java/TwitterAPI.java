7168b8e56d2512e5c08dcb13279af0063b523125
Dylan Fernandes <majestickookie@gmail.com>
2022-09-20 23:31:10 +0100

    CreateTweet method allows images to be uploaded along with a tweet

--- src/main/java/TwitterAPI.java
+++ src/main/java/TwitterAPI.java
@@ -1,7 +1,4 @@
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 
 import java.io.File;
 
@@ -12,16 +9,31 @@ public class TwitterAPI {
         return twitter;
     }
 
-    private static String createTweet(String tweet) throws TwitterException {
+    private static void createTweet(String tweet, File front, File frontShiny) throws TwitterException {
         Twitter twitter = getTwitterinstance();
-        Status status = twitter.updateStatus(tweet);
-        return status.getText();
+
+        // Uploading the Media for the tweet
+        UploadedMedia mediaFront = twitter.uploadMedia(front);
+        UploadedMedia mediaFrontShiny = twitter.uploadMedia(frontShiny);
+
+        // Getting the media Ids and long to upload to the status
+        long[] mediaIds = new long[2];
+        mediaIds[0] = mediaFront.getMediaId();
+        mediaIds[1] = mediaFrontShiny.getMediaId();
+
+
+        // Creating a new tweet with the message and mediaIds
+        StatusUpdate statusUpdate = new StatusUpdate(tweet);
+        statusUpdate.setMediaIds(mediaIds);
+        Status status = twitter.updateStatus(statusUpdate);
+
+        //return status.getText();
     }
 
-    public static void tweetContents(String message) throws TwitterException{
-        createTweet(message);
+    public static void tweetContents(String message, File front, File back) throws TwitterException{
+
+        createTweet(message, front, back);
 
     }
-
 
 }
